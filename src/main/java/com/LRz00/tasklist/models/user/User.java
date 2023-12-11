86200d5fa39376feb62dbe47e91470dbea535f45
Lara Rodrigues <lararods73@gmail.com>
2023-12-11 20:57:26 -0300

    created User model

--- src/main/java/com/LRz00/tasklist/models/user/User.java
+++ src/main/java/com/LRz00/tasklist/models/user/User.java
@@ -3,17 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.LRz00.tasklist.models.user;
- 
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
 /**
  *
  * @author lara
  */
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
 @Entity
 @Table(name = "user")
-
+@Data
 public class User {
+
+    public interface CreateUser {
+    }
+
+    public interface UpdateUser {
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    private String username;
+
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    private String password;
     
+    //private List<Tasks> = new ArrayList<Task>();
 }
