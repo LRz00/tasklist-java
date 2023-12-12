3d92d8737701f2cfee5cacfb9e6577b969b66653
Lara Rodrigues <lararods73@gmail.com>
2023-12-12 20:29:57 -0300

    Created User controller

---
+++ src/main/java/com/LRz00/tasklist/controllers/UserController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LRz00.tasklist.controllers;
+
+
+import com.LRz00.tasklist.models.User;
+import com.LRz00.tasklist.models.User.CreateUser;
+import com.LRz00.tasklist.models.User.UpdateUser;
+import com.LRz00.tasklist.services.UserService;
+import java.net.URI;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+/**
+ *
+ * @author lara
+ */
+@RestController
+@RequestMapping("/user/")
+@Validated
+public class UserController {
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = this.userService.findById(id);
+        return ResponseEntity.ok().body(user);               
+    }
+    
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody User user) {
+        User newUser = this.userService.create(user);
+        
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newUser.getId()).toUri();
+        
+        return ResponseEntity.created(uri).build();
+    }
+    
+    @PutMapping("/{id}")
+    @Validated(UpdateUser.class)
+    public ResponseEntity<Void> update(@Valid @RequestBody User user, @PathVariable Long id){
+        user.setId(id);
+        this.userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
