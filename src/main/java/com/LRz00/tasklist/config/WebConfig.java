9b26bface2b1d5c53f2da253715db6f4a4de01bd
Lara Rodrigues <lararods73@gmail.com>
2023-12-14 21:10:48 -0300

    created Web configuration file

---
+++ src/main/java/com/LRz00/tasklist/config/WebConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LRz00.tasklist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author lara
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+    
+    public void addCorsMapping(CorsRegistry registry){
+    registry.addMapping("/**");
+}
+}
