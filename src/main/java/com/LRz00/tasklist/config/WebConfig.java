88c4829e784a4c0799f698069d44daf26da23fd8
Lara Rodrigues <lararods73@gmail.com>
2023-12-16 18:21:21 -0300

    updates to WebConfig.java related to cors configuration

--- src/main/java/com/LRz00/tasklist/config/WebConfig.java
+++ src/main/java/com/LRz00/tasklist/config/WebConfig.java
@@ -7,6 +7,7 @@ package com.LRz00.tasklist.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -16,9 +17,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-    
-    public void addCorsMapping(CorsRegistry registry){
-    registry.addMapping("/**");
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") 
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
+    }
 }
-}
+
